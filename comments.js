81773b95a463ceb31646f4f6e6534b78ca99b2d8
Shreenath Vyas <v-shreenath.vyas@lightfinance.com>
2024-06-10 15:25:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,58 +1,31 @@
-// create web server 
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:3000
 
-// require express module
-const express = require('express');
-const app = express();
+var http = require('http');
+var url = require('url');
+var items = [];
 
-// require body-parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-// require mongoose
-const mongoose = require('mongoose');
-const Comment = require('./models/Comment');
-
-// connect to database
-mongoose.connect('mongodb://localhost/comments');
-
-// create a new comment
-app.post('/comments', (req, res) => {
-  console.log(req.body);
-  Comment.create(req.body, (err, comment) => {
-    console.log(err, comment);
-    res.json(comment);
-  });
+var server = http.createServer(function(req, res) {
+  switch (req.method) {
+    case 'POST':
+      var item = '';
+      req.setEncoding('utf8');
+      req.on('data', function(chunk) {
+        item += chunk;
+      });
+      req.on('end', function() {
+        items.push(item);
+        res.end('OK\n');
+      });
+      break;
+    case 'GET':
+      items.forEach(function(item, i) {
+        res.write(i + ') ' + item + '\n');
+      });
+      res.end();
+      break;
+  }
 });
 
-// get all comments
-app.get('/comments', (req, res) => {
-  Comment.find({}, (err, comments) => {
-    res.json(comments);
-  });
-});
-
-// get a single comment
-app.get('/comments/:id', (req, res) => {
-  Comment.findById(req.params.id, (err, comment) => {
-    res.json(comment);
-  });
-});
-
-// update a comment
-app.put('/comments/:id', (req, res) => {
-  Comment.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, comment) => {
-    res.json(comment);
-  });
-});
-
-// delete a comment
-app.delete('/comments/:id', (req, res) => {
-  Comment.findByIdAndRemove(req.params.id, (err, comment) => {
-    res.json(comment);
-  });
-});
-
-// start server
-app.listen(3000, () => {
-  console.log('Comments server listening on port 3000');
-});
+server.listen(3000);
